95b04554234e0ff9f7083116e8f18d01aee6fab9
苏恩泽 <991738965@qq.com>
2018-04-02 11:49:44 +0800

    4.2

--- account/src/main/java/com/haojishi/config/test.java
+++ account/src/main/java/com/haojishi/config/test.java
@@ -1,12 +1,5 @@
 package com.haojishi.config;
 
-import com.haojishi.controller.TransitionController;
-import org.springframework.http.HttpRequest;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
-
 public class test {
 
     public static void main(String[] args) {
