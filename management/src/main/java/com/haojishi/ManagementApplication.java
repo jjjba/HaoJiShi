b265df18cb50bd12db13ebe44dd142e88f8c206c
2529564015@qq.com <2529564015@qq.com>
2018-04-11 14:28:52 +0800

    修改跳转曾名称

--- management/src/main/java/com/haojishi/ManagementApplication.java
+++ management/src/main/java/com/haojishi/ManagementApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.haojishi.mapper")
 public class ManagementApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ManagementApplication.class, args);
 	}
